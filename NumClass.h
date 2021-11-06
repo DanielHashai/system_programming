8a1a8ecc145e265ca72c08f29c776cfbb8f84991
DanielHashai <aaronhashai@gmail.com>
2021-11-07 01:51:10 +0200

    added to  NumClass.h

--- NumClass.h
+++ NumClass.h
@@ -1,6 +1,8 @@
 #define FALSE 0
 #define TRUE 1
 
+
+int inOrder(int);
 /* will return if a number is Armstrong number
 An Armstrong number is an n-digit number that is equal to the sum of the nth powers of its digits.
 For Example: 407 = 43 + 03 + 73 = 64 + 0 + 343 = 407
