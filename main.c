bcc9efee85a328aca8e97eced0346c02ce261277
idan <idan.hashai@gmail.com>
2021-11-07 16:34:42 +0200

    print changes according to expected output

--- main.c
+++ main.c
@@ -9,34 +9,34 @@ int main() {
 
     int min = (num1 > num2) ? num2 : num1;
     int max = (num1 > num2) ? num1 : num2;
-    printf("Armstrong Numbers: ");
+    printf("The Armstrong numbers are: ");
     for (int i = min; i <= max; i++) {
         if (isArmstrong(i)) {
             printf("%d ", i);
         }
     }
     printf("\n");
-    printf("Palindrome Numbers: ");
+    printf("The Palindromes are:");
     for (int i = min; i <= max; i++) {
         if (isPalindrome(i)) {
-            printf("%d ", i);
+            printf(" %d", i);
         }
     }
     printf("\n");
  
-    printf("Prime Numbers: ");
+    printf("The Prime numbers are:");
     for (int i = min; i <= max; i++) {
         if (isPrime(i)) {
-            printf("%d ", i);
+            printf(" %d", i);
         }
     }
     printf("\n");
 
   
-    printf("Strong Numbers: ");
+    printf("The Strong numbers are: ");
     for (int i = min; i <= max; i++) {
         if (isStrong(i)) {
-            printf("%d ", i);
+            printf(" %d", i);
         }
     }
     printf("\n");
